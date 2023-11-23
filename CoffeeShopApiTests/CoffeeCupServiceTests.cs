1eaa921719c508fdfc586e1cd143b212d81561f5
LukasH0lm <lukasholm@hotmail.com>
2023-11-23 21:23:17 +0100

    Add Migrations and some files i forgor

---
+++ CoffeeShopApiTests/CoffeeCupServiceTests.cs
@@ -0,0 +1,86 @@
+
+using Models;
+using Presentation.DTOs;
+using Repository;
+using Service;
+
+namespace CoffeeShopApiTests;
+
+using AutoMapper;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+
+
+[TestFixture]
+public class CoffeeCupServiceTests
+{
+    private Mock<ICoffeeCupRepository> mockCoffeeCupRepository;
+    private Mock<IMapper> mockMapper;
+    private ICoffeeCupService coffeeCupService;
+
+    [SetUp]
+    public void Setup()
+    {
+        // Arrange
+        mockCoffeeCupRepository = new Mock<ICoffeeCupRepository>();
+        mockMapper = new Mock<IMapper>();
+
+        coffeeCupService = new CoffeeCupService(mockMapper.Object, mockCoffeeCupRepository.Object);
+    }
+
+    [Test]
+    public void GetCoffeeCupById_ShouldReturnCorrectCoffeeCup()
+    {
+        // Arrange
+        Guid coffeeCupId = Guid.NewGuid();
+        var coffeeCupEntity = new CoffeeCup { Id = coffeeCupId, Name = "Americano", Price = 3.99m };
+        var coffeeCupDto = new CoffeeCupDto { CupID = coffeeCupId, Name = "Americano", Price = 3.99m };
+
+        mockCoffeeCupRepository.Setup(repo => repo.GetCoffeeCupById(coffeeCupId)).Returns(coffeeCupEntity);
+        mockMapper.Setup(mapper => mapper.Map<CoffeeCupDto>(coffeeCupEntity)).Returns(coffeeCupDto);
+
+        // Act
+        var result = coffeeCupService.GetCoffeeCupById(coffeeCupId);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(coffeeCupDto.CupID, result.CupID);
+        Assert.AreEqual(coffeeCupDto.Name, result.Name);
+        Assert.AreEqual(coffeeCupDto.Price, result.Price);
+    }
+
+    [Test]
+    public void GetAllCoffeeCups_ShouldReturnAllCoffeeCups()
+    {
+        // Arrange
+        var coffeeCupEntities = new List<CoffeeCup>
+        {
+            new CoffeeCup { Id = Guid.NewGuid(), Name = "Latte", Price = 4.99m },
+            new CoffeeCup { Id = Guid.NewGuid(), Name = "Cappuccino", Price = 5.99m }
+        };
+
+        var coffeeCupDtos = new List<CoffeeCupDto>
+        {
+            new CoffeeCupDto { CupID = coffeeCupEntities[0].Id, Name = "Latte", Price = 4.99m },
+            new CoffeeCupDto { CupID = coffeeCupEntities[1].Id, Name = "Cappuccino", Price = 5.99m }
+        };
+
+        mockCoffeeCupRepository.Setup(repo => repo.GetAllCoffeeCups()).Returns(coffeeCupEntities);
+        mockMapper.Setup(mapper => mapper.Map<List<CoffeeCupDto>>(coffeeCupEntities)).Returns(coffeeCupDtos);
+
+        // Act
+        var result = coffeeCupService.GetAllCoffeeCups();
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(2, result.Count);
+        Assert.AreEqual(coffeeCupDtos[0].CupID, result[0].CupID);
+        Assert.AreEqual(coffeeCupDtos[1].Name, result[1].Name);
+        Assert.AreEqual(coffeeCupDtos[1].Price, result[1].Price);
+    }
+
+    // Add tests for AddCoffeeCup, UpdateCoffeeCup, and DeleteCoffeeCup methods as needed
+}
---
+++ Data/Migrations/20231123201835_InitialCreate.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CoffeeCup",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Size = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CoffeeCup", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    StockQuantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    IngredientID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    QuantityInStock = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CoffeeCupId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.IngredientID);
+                    table.ForeignKey(
+                        name: "FK_Ingredients_CoffeeCup_CoffeeCupId",
+                        column: x => x.CoffeeCupId,
+                        principalTable: "CoffeeCup",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CustomerID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TotalAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsApproved = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderID);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_CustomerID",
+                        column: x => x.CustomerID,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    OrderDetailID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrderID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ItemID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ItemType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Subtotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.OrderDetailID);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Orders_OrderID",
+                        column: x => x.OrderID,
+                        principalTable: "Orders",
+                        principalColumn: "OrderID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_CoffeeCupId",
+                table: "Ingredients",
+                column: "CoffeeCupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderID",
+                table: "OrderDetails",
+                column: "OrderID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerID",
+                table: "Orders",
+                column: "CustomerID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "CoffeeCup");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
